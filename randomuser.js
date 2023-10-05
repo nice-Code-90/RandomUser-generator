1e79d6ebe3c957f477bfc06c0445426f2415f200
zolttakacs <145469217+zolttakacs@users.noreply.github.com>
2023-10-05 13:53:45 +0200

    JS --- querySelector
    Script

---
+++ randomuser.js
@@ -0,0 +1,25 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const results = document.querySelector("[name=results]").value;
+    const nat = document.querySelector("[name=nat]").value;
+
+    fetch(`https://randomuser.me/api/?results=${results}&nat=${nat}`)
+        .then((result) => result.json())
+        .then((data) => {
+            const userList = data.results;
+            const markup = userList.map((user) => `
+                <div class="user m-3">
+                    <img src="${user.picture.large}" alt="Avatar of ${user.name.first} ${user.name.last}"/>
+                    <div>${user.name.first} ${user.name.last}</div>
+                    <div>
+                        ${user.email}
+                    </div>
+                    <div>
+                        ${user.location.city}, ${user.nat}
+                    </div>
+                </div>
+            `).join("");
+            document.querySelector(".js-users").innerHTML = markup;
+        });
+});
